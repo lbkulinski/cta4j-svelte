1b93cc54f22006ba004cce76838b034c972d06b2
Logan Kulinski <rashes_lineage02@icloud.com>
2024-11-27 22:37:42 -0600

    sveltestrap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fortawesome/fontawesome-free/css/all.min.css'
+	import {Nav, Navbar, NavbarBrand, NavLink} from "@sveltestrap/sveltestrap";
+
 	let { children } = $props();
 </script>
-<div class="p-2 flex justify-between">
-	<h1>cta4j</h1>
-	<nav>
-		<a href="/trains">TRAINS</a>
-		<a href="/buses">BUSES</a>
-		<a href="mailto:admin@cta4j.app">CONTACT</a>
-	</nav>
-</div>
+<Navbar class="py-0" color="dark" dark>
+	<NavbarBrand href="/">cta4j</NavbarBrand>
+	<Nav>
+		<NavLink class="text-white" href="/trains">
+			<i class="fa-solid fa-train-subway"></i>
+		</NavLink>
+		<NavLink class="text-white" href="/buses">
+			<i class="fa-solid fa-bus-simple"></i>
+		</NavLink>
+		<NavLink class="text-white" href="/holiday">
+			🎅
+		</NavLink>
+		<NavLink class="text-white" href="https://buymeacoffee.com/cta4j" target="_blank">
+			<i class="fa-solid fa-mug-hot"></i>
+		</NavLink>
+		<NavLink class="text-white" href="mailto:admin@cta4j.app">
+			<i class="fa-solid fa-envelope"></i>
+		</NavLink>
+	</Nav>
+</Navbar>
 {@render children()}
